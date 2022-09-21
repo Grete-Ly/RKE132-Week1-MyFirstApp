78ac8d95f3a958647731824a33d79c25967bd846
Grete <gseiler@tktk.ee>
2022-09-21 20:34:26 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See on kommentaar minu koodi jaoks
+
+// Rakendus küsib kasutajal sisestada tema nime
+// Rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+//string- sõne
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName} !"); ///Output
